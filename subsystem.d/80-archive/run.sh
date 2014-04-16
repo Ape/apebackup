51f61ee9070cbc3454bc06fbb8cdaef857c06331
Lauri Niskanen <ape@ape3000.com>
2014-04-16 21:52:54 +0300

    Add exclude file to the archiving subsystem
    The example now snapshots wine, but excludes Steam game library from the
    snapshots.

--- subsystem.d/80-archive/run.sh
+++ subsystem.d/80-archive/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-rsync -a --inplace --delete -r --files-from=target.txt / /mnt/archive
+rsync -a --inplace --delete -r --files-from=target.txt --exclude-from=exclude.txt / /mnt/archive
 snapper -c archive create -c timeline
 snapper -c archive cleanup timeline
